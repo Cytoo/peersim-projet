ca60cc3b14e7ca9f3cabd0ea22bd65e60a9fc328
Thomas Medioni <medioni.thomas@gmail.com>
2016-12-28 16:30:32 +0100

    Implement E2Q3: Nodes send and deliver probes messages
    Node send probes broadcasts every delta timer, update their neighbor list when receiving probes b4 timeouts.

--- src/projet/ElectionProtocolImpl.java
+++ src/projet/ElectionProtocolImpl.java
@@ -2,16 +2,30 @@ package projet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 import peersim.config.Configuration;
 import peersim.core.Node;
+import peersim.edsim.EDSimulator;
 
 public class ElectionProtocolImpl implements ElectionProtocol {
+	private static final String PAR_EMITTERID = "emitter";
+	private static final String PAR_TIMEOUT = "timeout";
+	
 	private final int protocol_id;
+	private final int emitter_id;
+	private final int timeout;
+	
+	private List<Long> neighbors = new ArrayList<Long>(); 
+	private List<Long> alive     = new ArrayList<Long>();
 
+	
+	
 	public ElectionProtocolImpl(String prefix) {
 		String tmp[] = prefix.split("\\.");
 		protocol_id = Configuration.lookupPid(tmp[tmp.length - 1]);
+		emitter_id = Configuration.getPid(prefix + "." + PAR_EMITTERID);
+		timeout = Configuration.getInt(prefix + "." + PAR_TIMEOUT);
 	}
 
 	@Override
@@ -19,6 +33,9 @@ public class ElectionProtocolImpl implements ElectionProtocol {
 		ElectionProtocolImpl ep = null;
 		try {
 			ep = (ElectionProtocolImpl) super.clone();
+			ep.neighbors = new ArrayList<Long>();
+			ep.alive	 = new ArrayList<Long>();
+			
 		} catch (CloneNotSupportedException e) {
 		} // never happens
 		return ep;
@@ -30,7 +47,21 @@ public class ElectionProtocolImpl implements ElectionProtocol {
 			throw new RuntimeException("Receive Message for wrong protocol");
 		}
 
-		if (event instanceof Message) {			
+		if (event instanceof ProbeMessage)
+		{
+			ProbeMessage msg = (ProbeMessage) event;
+			
+			if(!getNeighbors().contains(msg.getIdSrc()))
+			{
+				getNeighbors().add(msg.getIdSrc());
+			}
+			if(!alive.contains(msg.getIdSrc()))
+			{
+				alive.add(msg.getIdSrc());
+			}
+			
+		}
+		else if (event instanceof Message) {			
 			Message msg = (Message) event;
 
 			// If node is the receiver OR it is a broadcast
@@ -39,6 +70,25 @@ public class ElectionProtocolImpl implements ElectionProtocol {
 				//TODO
 			}
 		}
+		else if(event == null)
+		{
+			ListIterator<Long> it = getNeighbors().listIterator(); 
+			while(it.hasNext())
+			{
+				Long id = it.next();
+				if(!alive.contains(id))
+				{
+					it.remove();
+				}
+			}
+			alive.clear();
+			
+			Emitter em = (Emitter) node.getProtocol(emitter_id);
+			em.emit(node, new ProbeMessage(node.getID(), Emitter.ALL, "probe", null, protocol_id));
+			
+			EDSimulator.add(timeout, null, node, pid);
+		}
+		
 	}
 
 	@Override
@@ -61,7 +111,6 @@ public class ElectionProtocolImpl implements ElectionProtocol {
 
 	@Override
 	public List<Long> getNeighbors() {
-		// TODO Auto-generated method stub
-		return new ArrayList<Long>();
+		return neighbors;
 	}
 }
