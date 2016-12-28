fc31163842d7a93cb70d189a9c2a13465b7804e1
Thomas Medioni <medioni.thomas@gmail.com>
2016-12-28 14:33:09 +0100

    Question 2 basic implementation
    Implement emitter broadcast but not taking into account nodes proximity
    ElectionProtocol receives broadcasts and deliver if it is an applicative broadcast (Emitter.ADD) as dst or node.id

--- src/projet/ElectionProtocolImpl.java
+++ src/projet/ElectionProtocolImpl.java
@@ -3,32 +3,42 @@ package projet;
 import java.util.ArrayList;
 import java.util.List;
 
+import peersim.config.Configuration;
 import peersim.core.Node;
 
-
 public class ElectionProtocolImpl implements ElectionProtocol {
-	
+	private final int protocol_id;
 
-	public ElectionProtocolImpl(String prefix)
-	{
-		//TODO	
+	public ElectionProtocolImpl(String prefix) {
+		String tmp[] = prefix.split("\\.");
+		protocol_id = Configuration.lookupPid(tmp[tmp.length - 1]);
 	}
-	
+
 	@Override
-	public Object clone(){
-		ElectionProtocol ep = null;
-		try { ep = (ElectionProtocol) super.clone();
-			;
-		
-		}
-		catch( CloneNotSupportedException e ) {} // never happens
+	public Object clone() {
+		ElectionProtocolImpl ep = null;
+		try {
+			ep = (ElectionProtocolImpl) super.clone();
+		} catch (CloneNotSupportedException e) {
+		} // never happens
 		return ep;
 	}
 
 	@Override
 	public void processEvent(Node node, int pid, Object event) {
-		// TODO Auto-generated method stub
+		if (protocol_id != pid) {
+			throw new RuntimeException("Receive Message for wrong protocol");
+		}
 
+		if (event instanceof Message) {			
+			Message msg = (Message) event;
+
+			// If node is the receiver OR it is a broadcast
+			if (msg.getIdDest() == node.getID() || msg.getIdDest() == Emitter.ALL) {
+				//Then the message is delivered to the application
+				//TODO
+			}
+		}
 	}
 
 	@Override
@@ -54,5 +64,4 @@ public class ElectionProtocolImpl implements ElectionProtocol {
 		// TODO Auto-generated method stub
 		return new ArrayList<Long>();
 	}
-
 }
--- src/projet/EmitterImpl.java
+++ src/projet/EmitterImpl.java
@@ -1,42 +1,56 @@
 package projet;
 
+import peersim.config.Configuration;
+import peersim.core.Network;
 import peersim.core.Node;
+import peersim.edsim.EDSimulator;
 
 public class EmitterImpl implements Emitter {
 
+	private static final String PAR_ELECTION = "electionprotocol";
+	private static final String PAR_LATENCY = "latency";
+	private static final String PAR_SCOPE = "scope";
+
+	private final int election_protocol_id;
 	
-	public EmitterImpl(String prefix)
-	{
-		//TODO coder
+	private final int latency;
+	private final int scope;
+
+	public EmitterImpl(String prefix) {
+		election_protocol_id = Configuration.getPid(prefix + "." + PAR_ELECTION);
+		latency = Configuration.getInt(prefix + "." + PAR_LATENCY);
+		scope = Configuration.getInt(prefix + "." + PAR_SCOPE);
 	}
-	
+
 	@Override
 	public Object clone() {
 		EmitterImpl em = null;
-		try { em = (EmitterImpl) super.clone();
-		//TODO implement;
-		
-		}
-		catch( CloneNotSupportedException e ) {} // never happens
+		try {
+			em = (EmitterImpl) super.clone();
+		} catch (CloneNotSupportedException e) {
+		} // never happens
 		return em;
 	}
 
 	@Override
 	public void emit(Node host, Message msg) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0 ; i < Network.size() ; i++) {
+			Node n = Network.get(i);
+			// Vérifier que n est dans nos voisins
+			if (n != host) {
+				EDSimulator.add(this.getLatency(), msg, Network.get(i), election_protocol_id);
+			}
+		}
 	}
 
 	@Override
 	public int getLatency() {
-		// TODO Auto-generated method stub
-		return 0;
+		return latency;
 	}
 
 	@Override
 	public int getScope() {
-		// TODO Auto-generated method stub
-		return 0;
+		return scope;
 	}
 
 }
