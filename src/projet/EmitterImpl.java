f8776f954d45e2c5f14f0d9b10f5ab3df21f549f
Thomas Medioni <medioni.thomas@gmail.com>
2016-12-28 14:51:26 +0100

    Q2 end of implementation
    Proximity broadcast

--- src/projet/EmitterImpl.java
+++ src/projet/EmitterImpl.java
@@ -10,8 +10,10 @@ public class EmitterImpl implements Emitter {
 	private static final String PAR_ELECTION = "electionprotocol";
 	private static final String PAR_LATENCY = "latency";
 	private static final String PAR_SCOPE = "scope";
+	private static final String PAR_POSITION = "positionprotocol";
 
 	private final int election_protocol_id;
+	private final int position_pid;
 	
 	private final int latency;
 	private final int scope;
@@ -20,6 +22,7 @@ public class EmitterImpl implements Emitter {
 		election_protocol_id = Configuration.getPid(prefix + "." + PAR_ELECTION);
 		latency = Configuration.getInt(prefix + "." + PAR_LATENCY);
 		scope = Configuration.getInt(prefix + "." + PAR_SCOPE);
+		position_pid = Configuration.getPid(prefix + "." + PAR_POSITION);
 	}
 
 	@Override
@@ -36,9 +39,16 @@ public class EmitterImpl implements Emitter {
 	public void emit(Node host, Message msg) {
 		for (int i = 0 ; i < Network.size() ; i++) {
 			Node n = Network.get(i);
-			// Vérifier que n est dans nos voisins
+			
 			if (n != host) {
-				EDSimulator.add(this.getLatency(), msg, Network.get(i), election_protocol_id);
+				// Vérifier que n est dans nos voisins
+				PositionProtocol posNode = (PositionProtocol) n.getProtocol(position_pid);
+				PositionProtocol posHost = (PositionProtocol) host.getProtocol(position_pid);
+				double distance = Math.sqrt((posNode.getX() - posHost.getX()) * ((posNode.getX() - posHost.getX())) 
+											+(posNode.getY() - posHost.getY()) * ((posNode.getY() - posHost.getY())));
+				
+				if (distance < this.scope)
+					EDSimulator.add(this.getLatency(), msg, Network.get(i), election_protocol_id);
 			}
 		}
 	}
