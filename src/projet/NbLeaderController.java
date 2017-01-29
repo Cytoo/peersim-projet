f7547c5a2f41404fb211f250c51d872918964c35
alexandraHospital <hospital.alex@gmail.com>
2017-01-29 17:02:07 +0100

    without sysout

--- src/projet/NbLeaderController.java
+++ src/projet/NbLeaderController.java
@@ -32,8 +32,6 @@ public class NbLeaderController implements Control {
 		}
 		stddevm += (cur - (double)l / cpt) * (cur - (double)l / cpt);
 		 l+= cur;
-		System.out.println("mean number of leader" + (double)l / (cpt));
-		System.out.println("number of leader stddev: " + Math.sqrt(stddevm / cpt));
 		return false;
 	}
 
